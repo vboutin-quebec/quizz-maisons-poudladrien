49829fa235841798016701d27898d00f31c09047
vboutin-quebec <vboutin.quebec@gmail.com>
2025-06-16 08:56:27 -0400

    Update QuizPage.js

--- src/QuizPage.js
+++ src/QuizPage.js
@@ -1,1 +1,11 @@
-// Code du composant QuizPage inséré ici...
+// Code du composant QuizPage inséré ici...// src/QuizPage.js
+import React from 'react';
+
+export default function QuizPage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "serif" }}>
+      <h1>Quizz Maisons Poudladrien</h1>
+      <p>Le quiz est en construction. Revenez bientôt pour découvrir votre maison magique !</p>
+    </main>
+  );
+}
